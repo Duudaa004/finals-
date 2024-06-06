7fbe4851767a5747886205d7b78d7ef4bf29a42e
nugo jelia <166942516+Duudaa004@users.noreply.github.com>
2024-06-07 03:56:35 +0400

    add second section

--- script.js
+++ script.js
@@ -35,3 +35,17 @@ function nextSlide(){
     slideIndex++;
     showSlide(slideIndex);
 }
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) =>  observer.observe(el));
