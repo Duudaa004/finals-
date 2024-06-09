8d64818ca8782aaa68059a9956d7d3f06fcd1cca
nugo jelia <166942516+Duudaa004@users.noreply.github.com>
2024-06-10 01:56:24 +0400

    add last section and footer

--- script.js
+++ script.js
@@ -71,6 +71,9 @@ function showThird() {
     document.getElementById("secondAdviser").style.display = "none"
 }
 
+
+// projects
+
 function showAll() {
     document.getElementById("firstHidden").style.opacity = 1
     document.getElementById("secondHidden").style.opacity = 1
@@ -133,3 +136,99 @@ function showIcons() {
     document.getElementById("fifthHidden").style.opacity = 0
     document.getElementById("sixthHidden").style.opacity = 1
 }
+
+
+// input
+
+const statusField = document.createElement("span")
+statusField.setAttribute("id", "status_field")
+const contactForm = document.getElementById("contact_form_content")
+const formContainer = document.querySelector(".contact_form_bars_container")
+formContainer.appendChild(statusField)
+
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const name = document.getElementById("your_name")
+    const email = document.getElementById("email")
+    const website = document.getElementById("website")
+    const message = document.getElementById("message")
+
+    
+
+    try {
+        if (name.value.length < 6 ) {
+            name.style.border = "4px solid #E93656"
+            throw new Error("Name should contain at least 6 letters")
+        } else {
+            name.style.border = "4px solid green"
+        }
+
+        const validateEmail = (mail) => {
+            return String(mail)
+              .toLowerCase()
+              .match(  
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+          }
+
+          const isValidateEmail = validateEmail(email.value)
+          if (!isValidateEmail) {
+            email.style.border = "4px solid #E93656"
+            throw new Error("Email is not valid")
+          } else {
+            email.style.border = "4px solid green"
+          }
+
+          const validateWebsite = (web) => {
+            return String(web)
+              .toLowerCase()
+              .match(/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/)
+          }
+
+          const isValidateWebsite = validateWebsite(website.value)
+          if (!isValidateWebsite) {
+            website.style.border = "4px solid #E93656"
+            throw new Error("Website is not valid")
+          } else {
+            website.style.border = "4px solid green"
+          }
+
+          if (message.value.length < 100) {
+            message.style.border = "4px solid #E93656"
+            throw new Error("Message should contain at least 100 letters")
+          } else {
+            message.style.border = "4px solid green"
+          }
+          
+          if (name.style.border === "4px solid green" && email.style.border === "4px solid green" && website.style.border === "4px solid green" && message.style.border === "4px solid green") {
+            statusField.textContent = "Thank you, information sent successfully"
+            statusField.style.color = "green"
+            setTimeout(() => {
+                name.style.border = "none"
+                name.value = ""
+                email.style.border = "none"
+                email.value = ""
+                website.style.border = "none"
+                website.value = ""
+                message.style.border = "none"
+                message.value = ""
+                statusField.textContent = ""
+            }, 4000)
+          }
+
+    } catch (error) {
+        statusField.textContent = error.message
+    }
+
+    const formContent = new FormData(contactForm)
+    const info = Object.fromEntries(formContent)
+
+    fetch("https://borjomi.loremipsum.ge/api/send-message", {
+        method:"POST",
+        body: JSON.stringify(info)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .then(error => console.log(error))
+
+})
